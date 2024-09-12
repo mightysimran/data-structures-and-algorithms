a8a3fd7dce6dd0305a8f6664a3272a2cf04aef06
mightysimran <mightysimran.codes@gmail.com>
2024-09-12 18:25:43 +0530

    Pattern 6 -> Done

---
+++ 001 Patterns/pattern6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5-i;j++){
+            cout << j+1 << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
