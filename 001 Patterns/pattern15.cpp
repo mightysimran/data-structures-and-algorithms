038a392cc9cb19067397a66126f17b73b1d47ed8
mightysimran <mightysimran.codes@gmail.com>
2024-09-14 17:35:51 +0530

    Pattern 15 -> Done

---
+++ 001 Patterns/pattern15.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    for(int i=0;i<n;i++){
+        for(char ch='A';ch<='A'+(n-i-1);ch++){
+            cout << ch << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// A B C D E 
+// A B C D 
+// A B C 
+// A B 
+// A 
+// n=5
