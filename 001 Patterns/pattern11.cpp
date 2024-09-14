9c929cf14621b938c469fa466daf05cb26ede874
mightysimran <mightysimran.codes@gmail.com>
2024-09-14 17:05:38 +0530

    Pattern 11 -> Done

---
+++ 001 Patterns/pattern11.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int start;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            start = 1;
+        }
+        else{
+            start = 0;
+        }
+        for(int j=0;j<=i;j++){
+            cout << start << " ";
+            start = 1-start;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// 1 
+// 0 1 
+// 1 0 1 
+// 0 1 0 1 
+// 1 0 1 0 1 
+// n=5
