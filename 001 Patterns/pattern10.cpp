c0258928fe9e3e1da2d82d8bb668a030e256018b
mightysimran <mightysimran.codes@gmail.com>
2024-09-14 16:52:24 +0530

    Pattern 10 -> Done

---
+++ 001 Patterns/pattern10.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    for(int i=1; i<=2*n-1; i++){
+        int stars = i;
+        if(i>n){
+            stars = 2*n-i;
+        }
+        for(int j=1;j<=stars;j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// *
+// n=5
