d4c6feaaba4d57313b815b8c5c697fdca8995819
mightysimran <mightysimran.codes@gmail.com>
2024-09-14 17:23:02 +0530

    Pattern 13 -> Done

---
+++ 001 Patterns/pattern13.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int x = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout << x << " ";
+            x++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// n=4
