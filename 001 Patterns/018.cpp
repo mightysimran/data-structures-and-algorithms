1e6a4a9223efbaf170729eaee6867f60ba509e6b
mightysimran <mightysimran.codes@gmail.com>
2024-09-15 17:57:43 +0530

    Pattern 18 -> Done

---
+++ 001 Patterns/018.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    for(int i=0;i<n;i++){
+        for(char ch='E'-i;ch<='E';ch++){
+            cout << ch << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// E 
+// D E 
+// C D E 
+// B C D E 
+// A B C D E 
+// n=5
