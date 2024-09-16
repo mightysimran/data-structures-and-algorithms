73238ba5f557934f3fc6b1d84f6d9829e91c9829
mightysimran <mightysimran.codes@gmail.com>
2024-09-16 19:04:50 +0530

    pairs

---
+++ 002 C++ STL/pairs.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // pair<int, int> p = {1,5};
+    // cout << p.first << " " << p.second;
+
+    // pair<int, pair<int, int>> p = {1,{2,3}};
+    // cout << p.first << " " << p.second.first << " " << p.second.second;
+
+    // pair<int, int> arr[] = {{1,3},{2,5},{5,6}};
+    // cout << arr[1].second;
+
+    return 0;
+}
