51f6f6a56f312b085837a60ab4c9cb795a53ef1e
mightysimran <mightysimran.codes@gmail.com>
2024-10-02 23:19:22 +0530

    vectors

---
+++ 002 C++ STL/vectors.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // vector<int> v;
+
+    // v.push_back(1);
+    // v.emplace_back(2);
+
+    // vector<pair<int,int>> v;
+    // v.push_back({1,2});
+    // v.emplace_back(1,2);
+
+    // vector<int> v(5,100); // {100,100,100,100,100}
+
+    // vector<int> first(5,25); // {25,25,25,25,25}
+    // vector<int> second(first);
+
+    return 0;
+}
