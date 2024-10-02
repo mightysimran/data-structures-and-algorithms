e28926f2935160ed7c384dbcf21a6058936afe62
mightysimran <mightysimran.codes@gmail.com>
2024-10-02 23:39:11 +0530

    iterators

---
+++ 002 C++ STL/iterators.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // vector<int> v = {1, 2, 3, 4, 5};
+
+    // vector<int>::iterator i = v.begin(); // -> This iterator points to a memory location of first element 
+
+    // cout << *i << " "; // -> Element at zeroth index
+
+    // i++;
+    // cout << *(i) << " "; // -> Element at first index
+
+    // i = i + 2;
+    // cout << *(i) << " "; // -> Element at third index
+
+    // vector<int>::iterator i = v.end(); // -> This iterator points to a memory location that's right after last element
+
+    // vector<int>::iterator i = v.rend(); // -> This iterator points to a memory location that's right before first element 
+    // vector<int>::iterator i = v.rbegin(); // -> This iterator points to a memory location of last element
+
+    // v.rend() and v.rbegin() works in a reverse fashion.
+
+    // cout << v[0] << " " << v.at(0);
+    // cout << v.back();
+
+    return 0;
+}
