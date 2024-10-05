c3ec4e1c43fcbbcf7c58117246ea3594fedbdb34
mightysimran <mightysimran.codes@gmail.com>
2024-10-05 19:49:06 +0530

    deque

---
+++ 002 C++ STL/de-q.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // deque (double-ended queue) 
+    // deque<int> deq;
+    
+    // deq.push_front(2);
+    // deq.push_back(3);
+    // deq.emplace_front(1);
+    // deq.emplace_back(4);
+
+    // for(auto i: deq){
+    //     cout << i << " ";
+    // }
+
+    // deq.pop_back();
+    // deq.pop_front();
+
+    // cout << deq.back() << " ";
+    // cout << deq.front() << " ";
+
+    // rest fncs same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
+
+    return 0;
+}
