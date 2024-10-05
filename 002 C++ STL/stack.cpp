40347d5923c6b4e1a517faa63b213dbfb4b9dd83
mightysimran <mightysimran.codes@gmail.com>
2024-10-05 22:59:08 +0530

    stack

---
+++ 002 C++ STL/stack.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // stack<int> s; // There's nothing such as indexes here
+
+    // s.push(1); // {1}
+    // s.push(2); // {2, 1}
+    // s.push(3); // {3, 2, 1}
+    // s.push(3); // {3, 3, 2, 1}
+    // s.emplace(5); // {5, 3, 3, 2, 1}
+
+    // cout << s.top(); // returns 5
+    
+    // s.pop(); // s -> {3, 3, 2, 1}
+    // cout << s.top(); // returns 3
+    // cout << s.size(); // 4
+    // cout << s.empty(); // false
+
+    return 0;
+}
+
+// Stack works on "Last in, first out" method.
