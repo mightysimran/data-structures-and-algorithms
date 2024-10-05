448e09a44623c7fd1fce2179376ad442fe46e627
mightysimran <mightysimran.codes@gmail.com>
2024-10-05 19:19:51 +0530

    lists

---
+++ 002 C++ STL/lists.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // list<int> ls = {1, 2};
+
+    // ls.push_front(0); // {0, 1, 2}
+    // ls.push_back(3); // {0, 1, 2, 3} 
+
+    // for(auto i: ls){
+    //     cout << i << " ";
+    // }
+
+    // rest fncs same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
+
+    return 0;
+}
