f9a4bf1eac7825ac4f233aa7134a72dc5e85ea06
mightysimran <mightysimran.codes@gmail.com>
2024-10-05 22:59:20 +0530

    queue

---
+++ 002 C++ STL/queue.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // queue<int> q;
+
+    // q.push(1);
+    // q.push(2);
+    // q.emplace(4);
+
+    // q.back() += 5;
+    // cout << q.back(); // returns 9
+
+    // // q is {1, 2, 9}
+    // cout << q.front(); // returns 1
+    // q.pop(); // q -> {2, 9}
+    // cout << q.front(); // returns 2
+
+    return 0;
+}
+
+// Queue works on "First in, first out" method.
