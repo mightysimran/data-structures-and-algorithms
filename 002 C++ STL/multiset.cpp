8372d98e461e9d7371d5fd75174525597ae1d964
mightysimran <mightysimran.codes@gmail.com>
2024-10-06 23:18:07 +0530

    multiset

---
+++ 002 C++ STL/multiset.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // multiset<int> ms; // -> stores non distinct members, rest same as sets
+
+    // ms.insert(1); // {1}
+    // ms.insert(1); // {1, 1}
+    // ms.insert(1); // {1, 1, 1}
+
+    // // int count = ms.count(1) // returns 3
+    // // ms.erase(1); // {}
+    // // ms.erase(ms.find(1)); // {1, 1}
+
+    return 0;
+}
