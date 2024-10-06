3aecf22e2f1c8d6ed6fc96a475150cc0575a42bf
mightysimran <mightysimran.codes@gmail.com>
2024-10-06 22:52:13 +0530

    maximum and minimum heap

---
+++ 002 C++ STL/priority-queue.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // Maximum Priority Queue - Maximum Heap
+    // priority_queue<int> pq;
+
+    // pq.push(5); // {5}
+    // pq.push(2); // {5, 2}
+    // pq.push(8); // {8, 5, 2}
+    // pq.emplace(10); // {10, 8, 5, 2}
+
+    // cout << pq.top(); // returns 10
+    // pq.pop(); // pq -> {8, 5, 2}
+    // cout << pq.top(); // returns 8
+
+    // // Minimum Priority Queue - Minimum Heap
+    // priority_queue<int, vector<int>, greater<int>> pq;
+    // pq.push(5); // {5}
+    // pq.push(2); // {2, 5}
+    // pq.push(8); // {2, 5, 8}
+    // pq.emplace(10); // {2, 5, 8, 10}
+
+    // cout << pq.top();  // returns 2
+
+    return 0;
+}
