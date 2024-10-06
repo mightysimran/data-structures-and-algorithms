04c64c8c31f67079ea7d97ef3bc0e610116d879b
mightysimran <mightysimran.codes@gmail.com>
2024-10-06 23:06:08 +0530

    set

---
+++ 002 C++ STL/sets.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // set<int> s; // -> sorted order // -> distinct members // -> logarithmic time
+
+    // s.insert(1); // {1}
+    // s.insert(2); // {1, 2}
+    // s.insert(2); // {1, 2}
+    // s.insert(4); // {1, 2, 4}
+    // s.insert(3); // {1, 2, 3, 4}
+
+    // // {1, 2, 3, 4, 5}
+    // auto i = s.find(3); // -> returns an iterator (memory location)
+    // auto i = s.find(25); // -> returns s.end()
+
+    // s.erase(5); // erases 5
+    // // s.erase(i); // an iterator can also be an arg here
+
+    // int count = s.count(5); 
+
+    // // {1, 2, 3, 4, 5}
+    // auto x = s.find(2);
+    // auto y = s.find(5);
+    // // s.erase(x, y); // after erase -> {1, 5} [first, last)
+
+    // auto i = s.lower_bound(2);
+    // auto i = s.upper_bound(3);
+
+    return 0;
+}
