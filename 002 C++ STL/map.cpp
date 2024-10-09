50101df501fbf301df0a0e2856841743f1f0cd91
mightysimran <mightysimran.codes@gmail.com>
2024-10-09 23:53:41 +0530

    maps

---
+++ 002 C++ STL/map.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // map<int, string> m; 
+
+    // m[1] = "abc";
+    // m[3] = "lmn";
+    // m.insert({2, "def"});
+
+    // map<int, string>::iterator i;
+    // for (i = m.begin(); i != m.end(); i++){
+    //     cout << (*i).first << " " << (*i).second << endl;
+    // }
+
+    // for(auto &i: m){
+    //     cout << i.first << " " << i.second << endl;
+    // }
+
+    return 0;
+}
