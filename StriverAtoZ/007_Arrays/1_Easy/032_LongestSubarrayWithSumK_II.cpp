c8a79b7cb9aba719964cbea343a1a36e9300e3c0
mightysimran <mightysimran.codes@gmail.com>
2025-06-24 22:38:07 +0530

    Longest subarray with sum k (Positives) v2

---
+++ StriverAtoZ/007_Arrays/1_Easy/032_LongestSubarrayWithSumK_II.cpp
@@ -0,0 +1,33 @@
+// brute force
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+
+    int len=0;
+    for(int i=0; i<n; i++){
+        int sum=0;
+        for(int x=i; x<n; x++){
+            sum+=v[x];
+            if(k==sum){
+                len=max(len,x-i+1);
+            }
+        }
+    }
+
+    cout << len;
+
+    return 0;
+}
+
+// TC: n²
+// Space: 1
+// Problem: longest subarray with sum k
